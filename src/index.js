954a32641db53905f2e0e383668a25a34b9b79ad
AndrewTrahan <globalART19@gmail.com>
2018-05-01 15:16:47 -0500

    A: map and marker up and running!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('Or something')
+
+const mapboxgl = require('mapbox-gl')
+
+mapboxgl.accessToken = "pk.eyJ1IjoiZ2xvYmFsYXJ0MTkiLCJhIjoiY2pnbzNscXZjMGd3czJ4cXZ0enJ4cnU0OCJ9.cbTfjjRZ6_Pcbme46MQqrA"
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-87.639, 41.8954],
+  zoom: 12,
+  style: "mapbox://styles/mapbox/streets-v10"
+})
+
+const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-87.639, 41.8954]).addTo(map)
